41f265d7a64d869499bc6647463179f7772f1ae3
stoyangalchev <galchev98@gmail.com>
2024-11-05 19:35:46 +0200

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import pizzaRoute from "./routes/pizzaRoute.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Use routes
+app.use("/api/pizzas", pizzaRoute);
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
