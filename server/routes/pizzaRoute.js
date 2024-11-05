1b3a0b4584dd55533edc79907f79acfdea413016
stoyangalchev <galchev98@gmail.com>
2024-11-05 19:35:36 +0200

    Create pizzaRoute.js

---
+++ server/routes/pizzaRoute.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+
+import { getPizzas } from "../controller/pizza/getPizzas.js";
+import { filterPizza } from "../controller/pizza/getPizzas.js";
+import { postPizza } from "../controller/pizza/postPizza.js";
+
+
+router.get("/getpizzas", getPizzas);
+router.post("/filterpizzas", filterPizza);
+router.post("/addpizzas", postPizza);
+
+
+export default router;
