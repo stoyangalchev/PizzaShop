4148dd1d5be486c973c28ece5914facb20581758
stoyangalchev <galchev98@gmail.com>
2024-11-05 19:35:49 +0200

    Create getPizzas.js

---
+++ server/controller/pizza/getPizzas.js
@@ -0,0 +1,30 @@
+import Pizza from "../../models/pizzaModel.js";
+
+export const getPizzas = async (req, res) => {
+  try {
+    const pizzas = await Pizza.find({});
+    res.send(pizzas);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export const filterPizza = async (req, res) => {
+  const { category, query } = req.body;
+  let userPattern = new RegExp(query, "i");
+  try {
+    let pizzas;
+    if (!category && query) {
+      pizzas = await Pizza.find({ name: { $regex: userPattern } });
+    } else if (!query && category && category !== "all") {
+      pizzas = await Pizza.find({ category });
+    } else if (query && category) {
+      pizzas = await Pizza.find({ name: { $regex: userPattern }, category });
+    } else {
+      pizzas = await Pizza.find();
+    }
+    res.send(pizzas);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
