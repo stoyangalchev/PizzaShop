8b7496371679fccb45e80a8911f2ff45ea2f26ca
stoyangalchev <galchev98@gmail.com>
2024-11-07 13:20:58 +0200

    Create pizzaReducers.js

---
+++ pizzashop/src/reducers/pizzaReducers.js
@@ -0,0 +1,9 @@
+// src/reducers/index.js
+import { combineReducers } from "redux";
+// Import your reducers here
+
+const rootReducer = combineReducers({
+  // Add your reducers here
+});
+
+export default rootReducer;
