c1581dcfd62aa9810e048123a92a261c3a70477e
stoyangalchev <galchev98@gmail.com>
2024-11-18 15:59:56 +0200

    Update pizzaRoute.js

--- server/routes/pizzaRoute.js
+++ server/routes/pizzaRoute.js
@@ -5,10 +5,8 @@ import { getPizzas } from "../controller/pizza/getPizzas.js";
 import { filterPizza } from "../controller/pizza/getPizzas.js";
 import { postPizza } from "../controller/pizza/postPizza.js";
 
-
 router.get("/getpizzas", getPizzas);
 router.post("/filterpizzas", filterPizza);
 router.post("/addpizzas", postPizza);
-
 
 export default router;
