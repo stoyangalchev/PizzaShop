00aff581cd2ced3d50de2afca4bc75e88c3399af
stoyangalchev <galchev98@gmail.com>
2024-11-27 13:20:47 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import pizzaRoute from "./routes/pizzaRoute.js";
+import contactRoute from "./routes/contactRoute.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 // Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from this origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow cookies to be sent
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(express.json());
 
 // Database connection
@@ -20,8 +29,7 @@ mongoose
 
 // Use routes
 app.use("/api/pizzas", pizzaRoute);
-
-
+app.use("/api/contact", contactRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
