1923fe8c44417b32637d73eb0c426f2c09aa5633
stoyangalchev <galchev98@gmail.com>
2024-12-08 19:23:21 +0200

    Create App.jsx

---
+++ pizzashop/src/App.jsx
@@ -0,0 +1,125 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/shared/ScrollToTop';
+import Navbar from './components/navbar/Navbar';
+import Overlay from './components/home/Overlay';
+import Contact from './components/home/Contact';
+import Welcome from './components/home/Welcome';
+import OurService from './components/home/OurService';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import PizzaMeals from './components/home/PizzaMeals';
+import Images from './components/home/Images';
+import Status from './components/home/Status';
+import SmallMenu from './components/menu/SmallMenu';
+import Blog from './components/home/Blog';
+import ContactUs from './components/home/ContactUs';
+import AboutUs from './components/about/AboutUs';
+import OurMenu from './components/menu/OurMenu';
+import Services from './components/servicec/Services';
+import HotMeals from './components/servicec/HotMeals';
+import BlogHeader from './components/blog/BlogHeader';
+import AboutHeader from './components/about/AboutHeader';
+import ReadOurBlog from './components/blog/ReadOurBlog';
+import ContactHeader from './components/contact/ContactHeader';
+import ContactInformation from './components/contact/ContactInformation';
+import Cart from './components/cart/Cart'
+import ErrorBoundary from './components/shared/ErrorBoundary';
+import AdminLogin from './admin/AdminLogin'
+import AdminDashboard from './admin/AdminDashboard'
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: 'slide',
+    });
+
+
+
+
+  }, []);
+
+  return (
+    <Router>
+      <ScrollToTop />
+      <Navbar />
+      <ErrorBoundary>
+        <Routes>
+
+          <Route path="/" element={
+            <>
+              <Overlay />
+              <Contact />
+              <Welcome />
+              <OurService />
+              <PizzaMeals />
+              <Images />
+              <Status />
+              <SmallMenu />
+              <Blog />
+              <ContactUs />
+              <AboutUs />
+            </>
+          } />
+
+          <Route path="/menu" element={
+            <>
+              <OurMenu />
+              <PizzaMeals />
+              <SmallMenu />
+              <AboutUs />
+            </>
+          } />
+
+          <Route path="/services" element={
+            <>
+              <Services />
+              <OurService />
+              <HotMeals />
+              <AboutUs />
+            </>
+          } />
+
+          <Route path="/blog" element={
+            <>
+              <BlogHeader />
+              <Blog />
+              <ReadOurBlog />
+              <AboutUs />
+            </>
+          } />
+
+          <Route path="/about" element={
+            <>
+              <AboutHeader />
+              <Contact />
+              <Status />
+              <AboutUs />
+            </>
+          } />
+
+          <Route path="/contact" element={
+            <>
+              <ContactHeader />
+              <ContactInformation />
+              <AboutUs />
+            </>
+          } />
+
+          <Route path="/cart" element={
+            <>
+              <Cart />
+              <AboutUs />
+            </>
+          } />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
+        </Routes>
+      </ErrorBoundary>
+    </Router>
+  );
+}
+
+export default App;
