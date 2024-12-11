d10be85b241911dd22947823044bcc9ed2f029bc
stoyangalchev <galchev98@gmail.com>
2024-12-11 22:57:56 +0200

    Update pizzaRoute.js

--- server/routes/pizzaRoute.js
+++ server/routes/pizzaRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 const router = express.Router();
+import protect from "../middleware/protect.js";
 
 import { getPizzas } from "../controller/pizza/getPizzas.js";
 import { filterPizza } from "../controller/pizza/getPizzas.js";
 import { postPizza } from "../controller/pizza/postPizza.js";
 
-router.get("/getpizzas", getPizzas);
-router.post("/filterpizzas", filterPizza);
-router.post("/addpizzas", postPizza);
+router.get("/getpizzas",protect, getPizzas);
+router.post("/filterpizzas",protect, filterPizza);
+router.post("/addpizzas",protect, postPizza);
 
 export default router;
