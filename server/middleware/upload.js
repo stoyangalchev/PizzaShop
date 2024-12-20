557615c31348fa9c2d001b0f89f172cfd52767d9
stoyangalchev <galchev98@gmail.com>
2024-12-20 07:28:32 +0200

    Create upload.js

---
+++ server/middleware/upload.js
@@ -0,0 +1,33 @@
+import multer from "multer";
+import path from "path";
+
+// Set up multer storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// File filter to accept only images
+const fileFilter = (req, file, cb) => {
+  const fileTypes = /jpeg|jpg|png/;
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = fileTypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 1024 * 1024 * 50 }, // 50MB limit
+});
+
+export default upload;
