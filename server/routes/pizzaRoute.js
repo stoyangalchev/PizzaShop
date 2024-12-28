e35da66627429f8eded4f00dda208852ddc7697c
stoyangalchev <galchev98@gmail.com>
2024-12-28 15:34:42 +0200

    Update pizzaRoute.js

--- server/routes/pizzaRoute.js
+++ server/routes/pizzaRoute.js
@@ -5,9 +5,14 @@ import protect from "../middleware/protect.js";
 import { getPizzas } from "../controller/pizza/getPizzas.js";
 import { filterPizza } from "../controller/pizza/getPizzas.js";
 import { postPizza } from "../controller/pizza/postPizza.js";
+import { deletePizza } from "../controller/pizza/deletePizza.js";
+import { editPizza } from "../controller/pizza/editPizza.js";
+import upload from "../middleware/upload.js";
 
-router.get("/getpizzas", protect, getPizzas);
+router.get("/getpizzas", getPizzas);
 router.post("/filterpizzas", protect, filterPizza);
-router.post("/addpizzas", protect, postPizza);
+router.post("/addpizzas", upload.single("image"), protect, postPizza);
+router.put("/editpizzas/:id", upload.single("image"), protect, editPizza);
+router.delete("/deletepizzas/:id", protect, deletePizza);
 
 export default router;
