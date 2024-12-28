89ec469370c6ffa97e00c4bf7ae59f9b712ba973
stoyangalchev <galchev98@gmail.com>
2024-12-28 15:34:47 +0200

    Update main.jsx

--- pizzashop/src/main.jsx
+++ pizzashop/src/main.jsx
@@ -8,9 +8,9 @@ import store from './store/store';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
+
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+ 
 );
