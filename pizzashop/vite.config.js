1563254c5fc3190a1bf8fc2330910e880b2633f1
stoyangalchev <galchev98@gmail.com>
2025-01-22 20:11:37 +0200

    Update vite.config.js

--- pizzashop/vite.config.js
+++ pizzashop/vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -11,5 +10,8 @@ export default defineConfig({
         // Additional configuration options for SCSS
       },
     },
+  },
+  server: {
+    open: true, // Automatically open the browser on server start
   },
 });
