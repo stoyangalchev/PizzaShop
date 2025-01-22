c05c76acb3785b2bb60882cb4b8e2290af4aebbb
stoyangalchev <galchev98@gmail.com>
2025-01-22 19:41:55 +0200

    Update main.jsx

--- pizzashop/src/main.jsx
+++ pizzashop/src/main.jsx
@@ -1,16 +1,16 @@
-
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import React from 'react';
 import App from './App';
 import store from './store/store';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-
-    <Provider store={store}>
+  <Provider store={store}>
+    <SpeedInsights>
       <App />
-    </Provider>
- 
+    </SpeedInsights>
+  </Provider>
 );
